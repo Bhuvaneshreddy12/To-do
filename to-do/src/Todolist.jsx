de169c6a24774437af9bbf6bbc52a948acb9efb8
Bhuvanesh Reddy <bhuvanredy12@gmail.com>
2025-07-21 12:54:16 +0530

    Added css

--- to-do/src/Todolist.jsx
+++ to-do/src/Todolist.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 function Todolist(){
-    const [tasks,st]=useState(["eatbreakfast","Takeshower","gotoschool"]);
+    const [tasks,st]=useState(["Eat breakfast","Take shower","Goto school"]);
     const [newt,nst]=useState("");
     function hic(event){
         nst(event.target.value);
     }
     function addt(){
-
+        if(newt.trim()!=" "){
+        st(t=>[...t,newt]);
+        nst("");
+        }
     }
     function delt(index){
-
+        const updatet=tasks.filter((_,i)=>i!==index);
+        st(updatet);
     }
     function mtu(index){
-
+        const updatet=[...tasks];
+        if(index>0){
+        [updatet[index],updatet[index-1]]=[updatet[index-1],updatet[index]]
+        st(updatet);
+        }
     }
     function mdt(index){
+         const updatet=[...tasks];
+        if(index<tasks.length-1){
+        [updatet[index],updatet[index+1]]=[updatet[index+1],updatet[index]]
+        st(updatet);
+        }
 
     }
 return(<div className="to-do-list">
@@ -37,11 +50,11 @@ return(<div className="to-do-list">
                 delete
             </button>
          
-            <button className="move-btn" onClick={()=>mtu(index)}>
+            <button className="moveu-btn" onClick={()=>mtu(index)}>
                 👆
             </button>
          
-            <button className="delete-btn" onClick={()=>mdt(index)}>
+            <button className="moveu-btn" onClick={()=>mdt(index)}>
                 👇
             </button>
 
